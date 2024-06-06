0cf4d784f4c2fcaa20fa303a099f62fcd2189e76
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-06 17:24:39 +0200

    (seeders) populate db

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            'Laravel',
+            'PHP',
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'VueJS',
+            'TailwindCSS',
+            'Bootstrap',
+            'Sass'
+        ];
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Technology::generateSlug($new_technology->name);
+            $new_technology->save();
+        }
     }
 }
